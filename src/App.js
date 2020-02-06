af6fcab818ae25e3d36d32a75e915ab0ee551e95
Sony <mailsonytom@gmail.com>
2020-02-06 14:07:16 +0530

    Better way to do

--- src/App.js
+++ src/App.js
@@ -14,14 +14,23 @@ class App extends Component {
   };
 
  
-  nameChangedHandler =  (event) => {
-    this.setState ( {
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value , age: 29 },
-        { name: "Kevin", age: 22 }
-      ]
-    })
+  nameChangedHandler =  (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      returnp.id === id;
+    });
+
+    const person = {
+       ...this.state.persons[personIndex];
+    };
+
+    // const person = Object.assign({},this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+    
+    const persons =[...this.state.persons];
+    persons[personIndex] = person; 
+
+    this.setState ( {persons: persons});
   }
 
   deletePersonHandler = (personIndex) => {
@@ -52,7 +61,8 @@ togglePersonsHandler = () => {
           {this.state.persons.map((person,index) => {
             return <Person
             click={() => this.deletePersonHandler(index)}
-            name={person.name} age={person.age} key={person.id}/>
+            name={person.name} age={person.age} key={person.id}
+            changed={() => this.nameChangedHandler(event, person.id)}/>
           })}
         </div> 
       );
