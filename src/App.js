6ddc436b32007dbe16acf0e2b2486dce34a67d8a
Sony <mailsonytom@gmail.com>
2020-02-06 11:42:25 +0530

    Outputting persons using map fn

--- src/App.js
+++ src/App.js
@@ -51,14 +51,9 @@ togglePersonsHandler = () => {
     if(this.state.showPersons){
       persons =(
         <div>
-        <Person name={this.state.persons[0].name} 
-         age={this.state.persons[0].age} />
-         <Person name={this.state.persons[1].name} 
-         age={this.state.persons[1].age} 
-         click={this.switchNameHandler}
-         changed={this.nameChangedHandler}></Person>
-         <Person name={this.state.persons[2].name} 
-         age={this.state.persons[2].age} />
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age}/>
+          })}
         </div> 
       );
     }
