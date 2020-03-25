c48348e8ebfd318bb66fe75c474b596549e755e8
Sony <mailsonytom@gmail.com>
2020-03-25 14:47:36 +0530

    Ref method1

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,13 +6,20 @@ import Aux from '../../../hoc/Aux';
 
 
 class Person extends Component {
+
+componentDidMount(){
+    this.inputElement.focus();
+}
+
     render() {
         console.log('[Person.js] rendering');
         return (
             <Aux className="Person" style={styles}>
                 <div className="Person" >
                         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
-                        <input type="text" onChange={this.props.changed} value={this.props.name}></input>
+                        <input
+                        ref={(inputEl) => {this.inputElement = inputEl}}
+                        type="text" onChange={this.props.changed} value={this.props.name}></input>
                 </div>
             </Aux>
         );
